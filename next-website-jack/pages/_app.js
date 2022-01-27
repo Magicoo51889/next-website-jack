81ba2bbe97f790204fe69d38edb30e219ec55fc4
Magicoo51889 <jackot11f@gmail.com>
2022-01-27 11:59:10 +0000

    Update to index.js

--- next-website-jack/pages/_app.js
+++ next-website-jack/pages/_app.js
@@ -1,12 +1,8 @@
 import '../styles/globals.css'
-import myProjectsPage from '../next-website-jack/pages/myProjects';
-import index from '../next-website-jack/pages/index';
 
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-
-  
 }
 
 export default MyApp

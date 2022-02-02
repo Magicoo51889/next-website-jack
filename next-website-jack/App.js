bf9513615bde822428be5f41f1c0db8adad69f45
Magicoo51889 <jackot11f@gmail.com>
2022-02-02 09:56:39 +0000

    Update App.js

--- next-website-jack/App.js
+++ next-website-jack/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Home, myProjectsPage } from './pages/'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { ReactDOM } from 'react-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/index';
 
 const App = () => {
     return (
       <Router>
 		  <Navbar/>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='./myProjects' component={myProjectsPage} />
-        </Switch>
+        <Routes>
+          <Route path='/' exact element={Home} />
+          <Route path='./myProjects' element={myProjectsPage} />
+        </Routes>
       </Router>
     );
   }
